a76102266a67f0a869774b4826ff0bf91a9b2f92
mengxin <605706647@qq.com>
2019-06-18 15:23:57 +0800

    修改多线程打分服务

--- component-batch/src/main/scala/cherish/component/batch/sync/FingerGradeSyncImpl.scala
+++ component-batch/src/main/scala/cherish/component/batch/sync/FingerGradeSyncImpl.scala
@@ -1,8 +1,8 @@
 package cherish.component.batch.sync
 
 import java.io._
-import java.util.UUID
-import java.util.concurrent.Executors
+import java.util.{Date, UUID}
+import java.util.concurrent.{ExecutorService, Executors}
 
 import cherish.component.batch.service.{FingerGradeService, JpaSaveOrUpdateService}
 import cherish.component.config.BatchConfig
@@ -21,8 +21,8 @@ class FingerGradeSyncImpl(batchConfig : BatchConfig,
                           jpaSaveOrUpdateService: JpaSaveOrUpdateService)extends LoggerSupport{
 
 
-  var qualityScore :QualityScore = null
-  private lazy val executor = Executors.newFixedThreadPool(batchConfig.threadNum)
+//  private lazy val executor = Executors.newFixedThreadPool(batchConfig.threadNum)
+  private var flag = true
 
   /**
     * 捺印指纹质量评分定时任务
@@ -30,12 +30,13 @@ class FingerGradeSyncImpl(batchConfig : BatchConfig,
     */
   @PostInjection
   def startUp(periodicExecutor: PeriodicExecutor): Unit = {
-    if(batchConfig.sync.syncCron != null){
+    if(batchConfig.sync.syncCron != null && flag){
       periodicExecutor.addJob(new CronScheduleWithStartModel(batchConfig.sync.syncCron , StartAtDelay), "sync-cron", new Runnable {
         override def run(): Unit = {
           info("begin sync-cron")
           try{
-            doWork
+            flag = false
+            doWork()
           }catch{
             case e:Exception =>
               error("FingerGradeSyncImpl error:{}",e.getMessage,e)
@@ -47,44 +48,51 @@ class FingerGradeSyncImpl(batchConfig : BatchConfig,
   }
 
   def doWork(): Unit ={
+    val executor = Executors.newFixedThreadPool(batchConfig.threadNum)
     val tpcardimgmntList = Tpcardimgmnt.selectByA("select t.* from tpcardimgmnt t,personinfo p where t.flag = 0 and t.personid = p.personid and p.person_level is not null and rownum <=" + batchConfig.threadNum)
     logger.info("Tpcardimgmnt集合长度:" + tpcardimgmntList.size())
     System.loadLibrary("java_cherishqualityutil")
     //tpcardimgmnts 调用动态库打分，存入quality_score表
-    for(i <- 0 until(tpcardimgmntList.size())){
-      try {
-        val tpcard = tpcardimgmntList.get(i)
-        executorMethod(tpcard)
-      }catch {
-        case e:Exception =>
-          error("FingerGradeSyncImpl error:{}",e.getMessage,e)
-          e.printStackTrace()
+    try {
+      for(i <- 0 until(tpcardimgmntList.size())){
+          val tpcard = tpcardimgmntList.get(i)
+          executorMethod(tpcard)
       }
+      executor.shutdown()
+      while(true){
+        if(executor.isTerminated){
+          flag = true
+          return
+        }
+      }
+
+    }catch {
+      case e:Exception =>
+        error("FingerGradeSyncImpl error:{}",e.getMessage,e)
+        e.printStackTrace()
     }
+
     def executorMethod(tpcardimgmnt: Tpcardimgmnt): Unit ={
-      executor.execute(new Runnable {
+      executor.submit(new Runnable {
         override def run(): Unit = {
           val personId = tpcardimgmnt.personid
           logger.info("启动线程处理" + personId)
-          logger.info(personId + "开始解析imgData")
           val imgDataList = KryoListConvertUtils.deserializationList(tpcardimgmnt.imgData)
-          val imgDatas = imgDataList.iterator()
-          logger.info(personId + "解析imgData 完成,imgDataList长度：" + imgDataList.size())
-          qualityScore = new QualityScore(UUID.randomUUID().toString.replace("-", ""))
+          val qualityScore: QualityScore = new QualityScore(UUID.randomUUID().toString.replace("-", ""))
           var pScore: Double = 0
           //平指采集独立得分
           var rScore: Double = 0
           //滚指采集独立得分
-          val ftpPath = batchConfig.ftpPath + "/" + personId
-          while (imgDatas.hasNext) {
-            val imageData = imgDatas.next()
+          val ftpPath = batchConfig.ftpPath + personId
+          for(i <- 0 until(imgDataList.size())){
+            val imageData = imgDataList.get(i)
             if (null != imageData.mntData) {
               try {
-                logger.info(personId + "指位：" + imageData.fgp + "开始调用动态库打分")
+//                logger.info(personId + "指位：" + imageData.fgp + "开始调用动态库打分")
                 val fgp = if (imageData.fgp > 10) imageData.fgp - 10 else imageData.fgp
                 val qualityImage = NativeQualityScore.GetQualityScore(imageData.imgData, imageData.mntData, fgp, "QualityImage") //图像打分
                 logger.info(personId + "指位：" + imageData.fgp + "调用动态库打分结束:" + qualityImage.qualityScore)
-                qualityScore = qualityScoreSet(imageData.fgp, qualityImage.qualityScore, qualityScore) //QualityScore 给打完分的指位赋值
+                qualityScoreSet(imageData.fgp, qualityImage.qualityScore, qualityScore) //QualityScore 给打完分的指位赋值
                 saveImg(personId, imageData.fgp, imageData.imgData, qualityImage.qualityImg) //保存原图和红白图
               } catch {
                 case ex: Exception =>
@@ -92,7 +100,7 @@ class FingerGradeSyncImpl(batchConfig : BatchConfig,
               }
             } else {
               logger.info(personId + "指位：" + imageData.fgp + "没有特征，默认为0分")
-              qualityScore = qualityScoreSet(imageData.fgp, 0, qualityScore) //没有特征默认0分
+              qualityScoreSet(imageData.fgp, 0, qualityScore) //没有特征默认0分
             }
           }
 
@@ -120,8 +128,10 @@ class FingerGradeSyncImpl(batchConfig : BatchConfig,
           qualityScore.cardid = personId
           qualityScore.imgUrl = ftpPath
           qualityScore.totalScore = totalScore
+          logger.info("线程" + Thread.currentThread().getName +"qualityScore—"+ personId +" : "+qualityScore)
           jpaSaveOrUpdateService.qualityScoreSave(qualityScore)
           tpcardimgmnt.flag = 1
+          tpcardimgmnt.updatetime = new Date()
           jpaSaveOrUpdateService.tpcardimgmntUpdate(tpcardimgmnt)
 
           //根据设置分数判断人员指纹是否达标
